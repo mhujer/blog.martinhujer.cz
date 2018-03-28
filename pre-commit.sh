2aebf3c2919be75ab5a580ace93c8f2d941bdab1
Martin Hujer <mhujer@gmail.com>
2018-03-28 22:51:47 +0200

    fix pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,5 +4,5 @@ rm -rf output_prod
 rm -rf docs
 php ../sculpin/bin/sculpin generate --env=prod
 mkdir docs
-cp -R /c/devweb/htdocs/blog.martinhujer.cz/output_prod/* /c/devweb/htdocs/blog.martinhujer.cz/docs
+cp -R /c/dev/htdocs/blog.martinhujer.cz/output_prod/* /c/dev/htdocs/blog.martinhujer.cz/docs
 git add docs/
